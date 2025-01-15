da1cdf61e8d5bc0c7a22ba66d1429831f57e5683
Alberto <alberto.tobarra@gmail.com>
2025-01-15 17:34:08 +0000

    Add welcome and game pages with navigation; initiate game logic

--- app/src/main/java/com/example/hangman/MainActivity.kt
+++ app/src/main/java/com/example/hangman/MainActivity.kt
@@ -4,14 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.hangman.ui.theme.HangmanTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +34,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HangmanTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+               Surface(modifier = Modifier.fillMaxSize()){
+                   App()
                 }
             }
         }
@@ -31,17 +43,81 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun App() {
+    val navController = rememberNavController()
+
+    // Define NavHost with the required arguments
+    NavHost(
+        navController = navController,
+        startDestination = "welcomePage",
+    ) {
+        composable(route = "welcomePage") {
+            welcomePage(onNextScreen = {navController.navigate("game")})
+        }
+        composable (route = "game"){
+            game()
+        }
+    }
 }
+
+@Composable
+fun game() {
+    var word by remember {mutableStateOf("temporary")}
+    var progress by remember {mutableStateOf("")}
+    for(letter in word){
+        progress += ("_")
+
+    }
+    println(progress)
+    var InputLetter by remember {mutableStateOf("temporary")}
+
+    Column {
+        Text(text = "Hangman Game",
+            style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier
+                .padding(top = 100.dp))
+        Text(text = word,
+            style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier
+                .padding(top = 100.dp))
+    }
+}
+
+@Composable
+fun welcomePage(onNextScreen:()->Unit){
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally
+        ){
+        Text(
+            text = "Welcome to Hangman",
+            style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier
+                .padding(top = 100.dp)
+        )
+        Button(
+            onClick = {
+                onNextScreen()
+        },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 32.dp)
+        ){
+            Text(text = stringResource(R.string.button_label))
+        }
+    }
+}
+
+
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PreviewApp(){
     HangmanTheme {
-        Greeting("Android")
+        App()
     }
 }
